3901c5fc33a7a0f8d0f5c00d00211dca7b2fda63
Travis Ralston <travisr@matrix.org>
2022-05-19 22:38:22 -0600

    Hardcode the user directory to be disabled

--- synapse/handlers/user_directory.py
+++ synapse/handlers/user_directory.py
@@ -132,7 +132,8 @@ class UserDirectoryHandler(StateDeltasHandler):
         # Set of server names.
         self._is_refreshing_remote_profiles_for_servers: set[str] = set()
 
-        if self.update_user_directory:
+        # T2B: Disable user directory
+        if self.update_user_directory and False:
             self.notifier.add_replication_callback(self.notify_new_event)
 
             # We kick this off so that we don't have to wait for a change before
@@ -184,6 +185,11 @@ class UserDirectoryHandler(StateDeltasHandler):
 
     def notify_new_event(self) -> None:
         """Called when there may be more deltas to process"""
+
+        # T2B: Disable user directory
+        if True:
+            return
+
         if not self.update_user_directory:
             return
 
@@ -208,6 +214,10 @@ class UserDirectoryHandler(StateDeltasHandler):
         # FIXME(https://github.com/matrix-org/synapse/issues/3714): We should
         # probably do this in the same worker as all the other changes.
 
+        # T2B: Disable user directory
+        if True:
+            return
+
         if await self.store.should_include_local_user_in_dir(user_id):
             await self.store.update_profile_in_user_dir(
                 user_id, profile.display_name, profile.avatar_url
@@ -217,6 +227,11 @@ class UserDirectoryHandler(StateDeltasHandler):
         """Called when a user ID is deactivated"""
         # FIXME(https://github.com/matrix-org/synapse/issues/3714): We should
         # probably do this in the same worker as all the other changes.
+
+        # T2B: Disable user directory
+        if True:
+            return
+
         await self.store.remove_from_user_dir(user_id)
 
     async def _unsafe_process(self) -> None:
